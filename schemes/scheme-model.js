b71efd422bce373613651f645f75d2a8448cc61d
chiragthesia <harithesia@gmail.com>
2020-03-03 22:51:01 -0600

    MVP and Stretch Done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,6 +5,7 @@ module.exports = {
 	findById,
 	findSteps,
 	add,
+	addStep,
 	update,
 	remove
 };
@@ -15,21 +16,33 @@ function find() {
 
 function findById(id) {
 	//Finds schemes by id
-	return db('schemes').where('id', id);
+	return db('schemes').where('id', id).first();
 }
 
 function findSteps(id) {
 	//Finds steps by Id
+	return db('steps')
+		.join('schemes', 'schemes.id', 'steps.scheme_id')
+		.select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+		.where('scheme_id', id);
 }
 
 function add(scheme) {
 	//adds a new scheme
+	return db('schemes').insert(scheme);
+}
+
+function addStep(stepData, id) {
+	//adds a new step
+	return db('steps').update(stepData).where('scheme_id', id);
 }
 
 function update(changes, id) {
 	//updates current schemes etc.
+	return db('schemes').update(changes).where(id);
 }
 
 function remove(id) {
 	//removes a particular resource
+	return db('schemes').where('id', id).del();
 }
